66273a220bb813b22bd8a08044ff2381d988578f
InfinityLoop <swsoyee@gmail.com>
2018-02-25 16:56:33 +0900

    Add files via upload

---
+++ PythonDataVisualizationWeek3.py
@@ -0,0 +1,146 @@
+"""
+Project for Week 3 of "Python Data Visualization".
+Unify data via common country name.
+
+Be sure to read the project description page for further information
+about the expected behavior of the program.
+"""
+
+import csv
+import math
+import pygal
+
+def reconcile_countries_by_name(plot_countries, gdp_countries):
+    """
+    Inputs:
+      plot_countries - Dictionary whose keys are plot library country codes
+                       and values are the corresponding country name
+      gdp_countries  - Dictionary whose keys are country names used in GDP data
+
+    Output:
+      A tuple containing a dictionary and a set.  The dictionary maps
+      country codes from plot_countries to country names from
+      gdp_countries The set contains the country codes from
+      plot_countries that were not found in gdp_countries.
+    """
+    result_dict = {}
+    result_set = set()
+    for key, value in plot_countries.items():
+        try:
+            result_dict[key] = gdp_countries[value]['Country Name']
+        except KeyError:
+            result_set.add(key)
+    return result_dict, result_set
+
+def read_csv_as_nested_dict(filename, keyfield, separator, quote):
+    """
+    Inputs:
+      filename  - Name of CSV file
+      keyfield  - Field to use as key for rows
+      separator - Character that separates fields
+      quote     - Character used to optionally quote fields
+
+    Output:
+      Returns a dictionary of dictionaries where the outer dictionary
+      maps the value in the key_field to the corresponding row in the
+      CSV file.  The inner dictionaries map the field names to the
+      field values for that row.
+    """
+    nested_dict = {}
+    with open(filename, newline='') as csvfile:
+        reader = csv.DictReader(csvfile, delimiter=separator, quotechar=quote)
+        for row in reader:
+            nested_dict[row[keyfield]] = row
+    return nested_dict
+
+
+def build_map_dict_by_name(gdpinfo, plot_countries, year):
+    """
+    Inputs:
+      gdpinfo        - A GDP information dictionary
+      plot_countries - Dictionary whose keys are plot library country codes
+                       and values are the corresponding country name
+      year           - String year to create GDP mapping for
+
+    Output:
+      A tuple containing a dictionary and two sets.  The dictionary
+      maps country codes from plot_countries to the log (base 10) of
+      the GDP value for that country in the specified year.  The first
+      set contains the country codes from plot_countries that were not
+      found in the GDP data file.  The second set contains the country
+      codes from plot_countries that were found in the GDP data file, but
+      have no GDP data for the specified year.
+    """
+    result_dict = {}
+    empty_set = set()
+    database = read_csv_as_nested_dict(gdpinfo['gdpfile'], gdpinfo['country_name'],
+                                       gdpinfo['separator'], gdpinfo['quote'])
+    #within_dict, without_set = reconcile_countries_by_name(plot_countries, database)
+    within_dict = {}
+    without_set = set()
+    for key, value in plot_countries.items():
+        try:
+            within_dict[key] = database[value][gdpinfo['country_name']]
+        except KeyError:
+            without_set.add(key)
+    
+    for key, value in within_dict.items():
+        try:
+            result_dict[key] = math.log10(float(database[value][year]))
+        except (TypeError, ValueError):
+            empty_set.add(key)
+    return result_dict, without_set, empty_set
+
+def render_world_map(gdpinfo, plot_countries, year, map_file):
+    """
+    Inputs:
+      gdpinfo        - A GDP information dictionary
+      plot_countries - Dictionary whose keys are plot library country codes
+                       and values are the corresponding country name
+      year           - String year to create GDP mapping for
+      map_file       - Name of output file to create
+
+    Output:
+      Returns None.
+
+    Action:
+      Creates a world map plot of the GDP data for the given year and
+      writes it to a file named by map_file.
+    """
+    return
+
+
+def test_render_world_map():
+    """
+    Test the project code for several years.
+    """
+    gdpinfo = {
+        "gdpfile": "isp_gdp.csv",
+        "separator": ",",
+        "quote": '"',
+        "min_year": 1960,
+        "max_year": 2015,
+        "country_name": "Country Name",
+        "country_code": "Country Code"
+    }
+
+    # Get pygal country code map
+    pygal_countries = pygal.maps.world.COUNTRIES
+
+    # 1960
+    render_world_map(gdpinfo, pygal_countries, "1960", "isp_gdp_world_name_1960.svg")
+
+    # 1980
+    render_world_map(gdpinfo, pygal_countries, "1980", "isp_gdp_world_name_1980.svg")
+
+    # 2000
+    render_world_map(gdpinfo, pygal_countries, "2000", "isp_gdp_world_name_2000.svg")
+
+    # 2010
+    render_world_map(gdpinfo, pygal_countries, "2010", "isp_gdp_world_name_2010.svg")
+
+
+# Make sure the following call to test_render_world_map is commented
+# out when submitting to OwlTest/CourseraTest.
+
+# test_render_world_map()
